ff1253ea377ce13d808f62c9d207a14a30adb4b8
phil-gab99 <phil-gab99@users.noreply.github.com>
2020-10-29 16:29:14 -0400

    Provided comment header with minor todos

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,14 @@
+/*
+* @Karim Boumghar
+* @Philippe Gabriel
+* @Version 1.0 2020-11-12
+*
+* This program
+**/
+
+//todo : Give program specification
+//todo : Begin
+
 public class Test {
 
     public static void main(String[] args) {

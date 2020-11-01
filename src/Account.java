3d99281d5a3fe852bd9d6bd1b6835ff7fda3783d
phil-gab99 <phil-gab99@users.noreply.github.com>
2020-11-01 16:12:59 -0500

    Minor changes

---
+++ src/Account.java
@@ -0,0 +1,30 @@
+/*
+* @Karim Boumghar
+* @Philippe Gabriel
+* @Version 1.0 2020-11-12
+*
+* The class Account
+**/
+
+//todo : Give program specification
+//todo : Begin
+
+public abstract class Account {
+
+    private int balance; //Balance associated with the account
+
+    // public Account() {
+    // }
+
+    public void addBalance(int amount) {
+        this.balance += amount;
+    }
+    //
+    // public void removeBalance(int amount) {
+    //
+    // }
+
+    public int getBalance() {
+        return balance;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person extends Account {
+
+    private String firstName;
+    private String lastName;
+    private String birth;
+    private String email;
+
+    // public String generateEmail() {
+    //
+    // }
+
+}
